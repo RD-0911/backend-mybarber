de2a42168301a4b0958165a02053e5e2772268e8
Raul Dolores <rauldolores030@gmail.com>
2026-04-13 22:24:03 -0600

    fix: agregar SSL y puerto para Aiven

--- config/db.js
+++ config/db.js
@@ -2,22 +2,25 @@ const mysql = require("mysql2");
 
 const db = mysql.createPool({
   host:               process.env.DB_HOST,
+  port:               parseInt(process.env.DB_PORT),
   user:               process.env.DB_USER,
   password:           process.env.DB_PASSWORD,
   database:           process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit:    10,
+  ssl: {
+    rejectUnauthorized: true,
+  },
 });
 
 // Verificar conexión al iniciar
 db.getConnection((err, connection) => {
   if (err) {
-    console.error(" Error al conectar con MySQL:", err.message);
+    console.error("Error al conectar con MySQL:", err.message);
   } else {
     console.log("Conectado a la base de datos:", process.env.DB_NAME);
     connection.release();
   }
 });
 
-// Exportar versión con promesas para usar async/await
 module.exports = db.promise();
