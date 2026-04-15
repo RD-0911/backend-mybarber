b9bf4c69e39bd1672f87c04dd3a6d30c32430ce5
Raul Dolores <rauldolores030@gmail.com>
2026-04-14 18:54:17 -0600

    fix: cambiar puerto SMTP a 2525

--- config/mailer.js
+++ config/mailer.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
+  port: 2525,
   secure: false,
   auth: {
     user: "a8264a001@smtp-brevo.com",
